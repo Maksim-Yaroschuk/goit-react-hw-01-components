a2e5c27a4faa38ef230d8877e7a6a5a048d0d628
Maksim-Yaroschuk <79583144+Maksim-Yaroschuk@users.noreply.github.com>
2022-08-07 21:43:31 +0300

    to render profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import { Profile } from "./Profile/Profile"
+import user from 'user.json'
+
 export const App = () => {
   return (
     <div
@@ -10,7 +13,8 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile key={user.tag} user={user}
+      />
     </div>
   );
 };
